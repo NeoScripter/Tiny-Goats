de01ac435a74206c82f61a200d7ccf8b6a3942ce
Ilya <sange0337@gmail.com>
2024-10-21 22:23:41 +0800

    finished the home page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -36,7 +36,7 @@
                     <div class="goals__viewport" :style="{ transform: `translateX(${currentSlide * -100}%)` }">
                         @for ($i = 1; $i < 6; $i++)
                             <div class="goals__image">
-                                <img src="{{ asset("images/pages/home/index-slide-$i.webp") }}" alt="image">
+                                <img src="{{ asset("images/pages/user/home/goals/index-slide-$i.webp") }}" alt="image">
                             </div>
                         @endfor
                     </div>
@@ -52,6 +52,142 @@
                 </div>
 
             </div>
+
+        </section>
+
+        <section class="animals">
+
+            <div class="animals__headline">
+                <h2 class="animals__title">Животные</h2>
+
+                <button class="animals__btn">Смотреть все</button>
+            </div>
+
+            <div class="animals__wrapper" x-data="{ currentSlide: 0, totalSlides: 8 }">
+                <button class="animals__controller animals__btn--prev" x-show="currentSlide !== 0" @click="currentSlide = currentSlide - 1">
+                    {!! file_get_contents(public_path('images/svgs/prev-btn-dark.svg')) !!}
+                </button>
+                <div class="animals__carousel">
+                    <div class="animals__viewport" :style="{ transform: `translateX(${currentSlide * -25}%)` }">
+                        @for ($i = 1; $i < 9; $i++)
+                            <div class="animals__item">
+                                <div class="animals__image">
+                                    <img src="{{ asset("images/pages/user/home/animals/animal-$i.webp") }}" alt="image">
+                                </div>
+                                <h4 class="animals__name">Кличка</h4>
+                                <p class="animals__breed">Порода</p>
+                            </div>
+                        @endfor
+                    </div>
+                </div>
+                <button class="animals__controller animals__btn--next"  x-show="currentSlide !== (totalSlides - 4)" @click="currentSlide = currentSlide + 1">
+                    {!! file_get_contents(public_path('images/svgs/next-btn-dark.svg')) !!}
+                </button>
+            </div>
+
+        </section>
+
+        <section class="farm">
+
+            <div class="farm__headline">
+                <h2 class="farm__title">Хозяйства</h2>
+
+                <button class="farm__btn">Смотреть все</button>
+            </div>
+
+            <div class="farm__wrapper" x-data="{ currentSlide: 0, totalSlides: 6 }">
+                <button class="farm__controller farm__btn--prev" x-show="currentSlide !== 0" @click="currentSlide = currentSlide - 1">
+                    {!! file_get_contents(public_path('images/svgs/prev-btn-dark.svg')) !!}
+                </button>
+                <div class="farm__carousel">
+                    <div class="farm__viewport" :style="{ transform: `translateX(${currentSlide * -33}%)` }">
+                        @for ($i = 1; $i < 7; $i++)
+                            <div class="farm__item">
+                                <div class="farm__image">
+                                    <img src="{{ asset("images/pages/user/home/farm/farm-$i.webp") }}" alt="image">
+                                </div>
+                                <h4 class="farm__name">Хозяйство 3</h4>
+                                <p class="farm__place">Регион</p>
+                                <p class="farm__description">Описание</p>
+                            </div>
+                        @endfor
+                    </div>
+                </div>
+                <button class="farm__controller farm__btn--next"  x-show="currentSlide !== (totalSlides - 3)" @click="currentSlide = currentSlide + 1">
+                    {!! file_get_contents(public_path('images/svgs/next-btn-dark.svg')) !!}
+                </button>
+            </div>
+
+        </section>
+
+        <section class="news">
+
+            <div class="news__headline">
+                <h2 class="news__title">Новости и статьи</h2>
+
+                <button class="news__btn">Смотреть все</button>
+            </div>
+
+
+                <div class="news__grid">
+                    @for ($i = 1; $i < 5; $i++)
+                        <div class="news__item">
+                            <div class="news__image">
+                                <img src="{{ asset("images/pages/user/home/news/news-$i.webp") }}" alt="image">
+                                <span class="news__label">Раздел новости</span>
+                            </div>
+                            <div class="news__content">
+                                <h4 class="news__heading">Заголовок последней новости в несколько строк</h4>
+                                <p class="news__description">Описание в 1-2 строчки</p>
+                            </div>
+                        </div>
+                    @endfor
+                </div>
+
+        </section>
+
+        <section class="faq">
+
+            <h2 class="faq__title">Ответы на популярные вопросы</h2>
+
+            <p class="faq__subtitle">Предлагаем ознакомиться с ответами на самые популярные вопросы, а так же <a href="" class="faq__link">правилами</a> нашего сайта “Реестр карликовых коз России”</p>
+
+            <ul class="faq__list">
+                <li class="faq__li">
+                    <h3 class="faq__heading">Как я могу зарегистрировать/перерегистрировать, обновить карточку животного, или сменить владельца?</h3>
+                    <p class="faq__content">Для регистрации/перерегистрации, смены владельца и обновления карточки животного вам нужно отправить запрос Администратору по указанным на сайте контактам.</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">Какая цена регистрации  животных в Реестре?</h3>
+                    <p class="faq__content">Животные регистрируются бесплатно.  Возможно добровольное пожертвование в виде доната.</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">На каком языке осуществляется регистрация животных?</h3>
+                    <p class="faq__content">Регистрация животных, рождённых в России, осуществляется на русском языке (кириллицей). Регистрация импортных животных осуществляется на  английском языке (латинницей).</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">На какой срок можно зарегистрировать животное в каталоге?</h3>
+                    <p class="faq__content">Животное в Реестре регистрируется единожды и остаётся в нём на постоянной основе.</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">Как зарегистрировать хозяйство?</h3>
+                    <p class="faq__content">Для регистрации или обновления карточки хозяйства нужно отправить запрос Администратору по указанным на сайте контактам.</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">Какая цена регистрации хозяйства в Реестре?</h3>
+                    <p class="faq__content">Регистрация хозяйств осуществляется бесплатно. Возможно добровольное пожертвование в виде доната.</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">На каком языке регистрируются хозяйства в Реестре?</h3>
+                    <p class="faq__content">Регистрация хозяйств осуществляется на русском языке.</p>
+                </li>
+                <li class="faq__li">
+                    <h3 class="faq__heading">Какие дополнительные сведения о владельце можно внести в карточку хозяйства?</h3>
+                    <p class="faq__content">Любые, которые могут дать больше информации о владельце. Например, наличие профильного образования или с какого года ведётся работа с козами в хозяйстве и т.п.</p>
+                </li>
+            </ul>
+
+            <a href="" class="faq__btn">Читать полностью</a>
 
         </section>
 
